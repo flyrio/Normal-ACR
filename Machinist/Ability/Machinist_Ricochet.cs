4bd7fd1e76944f79c7cb704c9401649e0110fcdd
Shiyuvi <1057628603@qq.com>
2023-11-23 16:01:51 +0800

    Add files via upload

--- Machinist/Ability/Machinist_Ricochet.cs
+++ Machinist/Ability/Machinist_Ricochet.cs
@@ -10,7 +10,8 @@ public class Machinist_Ricochet : ISlotResolver
 
     public int Check()
     { //弹射层数大于虹吸弹层数.弹射好了
-        if (!Qt.GetQt("攒资源") && Core.Me.ClassLevel >= 50&& Core.Get<IMemApiSpell>().GetCharges(2890) > Core.Get<IMemApiSpell>().GetCharges(2874)) return 1;
+        if (!Qt.GetQt("攒资源") && Core.Me.ClassLevel >= 50 && Core.Me.ClassLevel < 74 && Core.Get<IMemApiSpell>().GetCharges(2890) >= 0.667 && Core.Get<IMemApiSpell>().GetCharges(2890) > Core.Get<IMemApiSpell>().GetCharges(2874)) return 1;
+        if (!Qt.GetQt("攒资源") && Core.Me.ClassLevel >= 74 && Core.Get<IMemApiSpell>().GetCharges(2890) >= 1 && Core.Get<IMemApiSpell>().GetCharges(2890) > Core.Get<IMemApiSpell>().GetCharges(2874)) return 1;
         return -1;
     }
     
--- Machinist/GCD/Machinist_Hypercharge.cs
+++ Machinist/GCD/Machinist_Hypercharge.cs
@@ -18,23 +18,23 @@ public class Machinist_Hypercharge : ISlotResolver
     public int Check()
     {
         //不在过热状态，热量大于50时，还没学会野火，无脑丢
-        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 35 && Core.Me.ClassLevel < 45 && !SpellsDefine.Hypercharge.RecentlyUsed(10000)
+        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 35 && Core.Me.ClassLevel < 45 && SpellsDefine.Hypercharge.IsReady()
             &&
-            SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 7000) return 0;
+            SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 5000) return 0;
         //不在过热，热量大于50，学会野火了，野火马上好了丢
         //不在过热，热量大于50，学会野火了，野火冷却30秒以上丢
-        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 45 && Core.Me.ClassLevel < 58 && !SpellsDefine.Hypercharge.RecentlyUsed(10000)
+        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 45 && Core.Me.ClassLevel < 58 && SpellsDefine.Hypercharge.IsReady()
             &&
-            SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 7000) return 2;
-        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 58 && Core.Me.ClassLevel < 76 && !SpellsDefine.Hypercharge.RecentlyUsed(10000)
+            SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 5000) return 2;
+        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 58 && Core.Me.ClassLevel < 76 && SpellsDefine.Hypercharge.IsReady()
             &&
-            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 7000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 7000 )) return 2;
-        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 76 && Core.Me.ClassLevel < 90 && !SpellsDefine.Hypercharge.RecentlyUsed(10000)
+            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 5000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 5000 )) return 2;
+        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel >= 76 && Core.Me.ClassLevel < 90 && SpellsDefine.Hypercharge.IsReady()
             &&
-            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 7000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 7000 || SpellsDefine.AirAnchor.GetSpell().Cooldown.TotalMilliseconds <= 7000 )) return 2;
-        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel == 90 && !SpellsDefine.Hypercharge.RecentlyUsed(10000)
+            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 5000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 5000 || SpellsDefine.AirAnchor.GetSpell().Cooldown.TotalMilliseconds <= 5000 )) return 2;
+        if (!Qt.GetQt("攒资源") && Core.Get<IMemApiMCH>().GetHeat() >=50 && !Core.Get<IMemApiMCH>().OverHeated() && Core.Me.ClassLevel == 90 && SpellsDefine.Hypercharge.IsReady()
             &&
-            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 7000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 7000 || SpellsDefine.AirAnchor.GetSpell().Cooldown.TotalMilliseconds <= 7000  || SpellsDefine.ChainSaw.GetSpell().Cooldown.TotalMilliseconds <= 7000)) return 2;
+            (SpellsDefine.HotShot.GetSpell().Cooldown.TotalMilliseconds <= 5000 || SpellsDefine.Drill.GetSpell().Cooldown.TotalMilliseconds <= 5000 || SpellsDefine.AirAnchor.GetSpell().Cooldown.TotalMilliseconds <= 5000  || SpellsDefine.ChainSaw.GetSpell().Cooldown.TotalMilliseconds <= 5000)) return 2;
         return -1;
     }
 
